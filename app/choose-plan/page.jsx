5345b00b6aaf6fd2cb5ab040f216be37c8805369
sebastian <manijustworkhere@gmail.com>
2025-12-17 14:37:26 -0500

    for vercel to stop crying

---
+++ app/choose-plan/page.jsx
@@ -0,0 +1,9 @@
+// app/choose-plan/page.jsx
+export default function ChoosePlanPage() {
+  return (
+    <div>
+      <h1>Choose Your Plan</h1>
+      <p>Content goes here...</p>
+    </div>
+  );
+}
